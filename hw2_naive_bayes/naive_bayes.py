334cb26731fb967c527cb19f58fc39f574baf6a1
George Manos <watchmedoctor@gmail.com>
2022-10-23 21:17:27 +0300

    Finished NB for continuous variables

--- hw2_naive_bayes/naive_bayes.py
+++ hw2_naive_bayes/naive_bayes.py
@@ -4,12 +4,20 @@ from tqdm import tqdm
 
 CATEGORICAL = "categorical"
 CONTINUOUS = "continuous"
-CLASS_PROBABILITIES = "class_probabilities"
-LIKELIHOODS = "likelihoods"
+CLASS_PROBABILITIES_KEY = "class_probabilities"
+LIKELIHOODS_KEY = "likelihoods"
+MEAN_KEY = "mean"
+STD_KEY = "std"
 TRAIN_SET_PERCENTAGE = 0.75
-PRINT_MODEL = False
+PRINT_MODEL = True
 REPEAT = 100
 SMOOTH_STR = 0
+
+DISCRETE_X_FP = 'Assignment2_Data/DatasetA_X_categorical.csv'
+DISCRETE_Y_FP = 'Assignment2_Data/DatasetA_Y.csv'
+D_CATEGORICAL_FP = 'Assignment2_Data/DatasetA_D_categorical.csv'
+CONTINUOUS_X_FP = 'Assignment2_Data/DatasetB_X_continuous.csv'
+CONTINUOUS_Y_FP = 'Assignment2_Data/DatasetB_Y.csv'
 
 
 def train_NBC(X: pd.DataFrame, X_dtype, Y: pd.DataFrame, L, D_categorical):
@@ -32,37 +40,45 @@ def train_NBC(X: pd.DataFrame, X_dtype, Y: pd.DataFrame, L, D_categorical):
     """
     sample_count = len(X)
     categories = np.unique(Y)
-    model = None
+    class_probability = {}
+    likelihoods = {}
 
-    if X_dtype is CATEGORICAL:
-        class_probability = {}
-        likelihoods = {}
+    for label in categories:
+        # 1. Calculate class prior probabilities
+        label_count = len(Y[Y[0] == label])
+        class_probability[label] = (label_count + L) / (sample_count + L * len(categories))
+        class_x = X[Y[0] == label]
 
-        for label in categories:
-            # 1. Calculate class prior probabilities
-            label_count = len(Y[Y[0] == label])
-            class_probability[label] = (label_count + L) / (sample_count + L * len(categories))
-            class_x = X[Y[0] == label]
-
-            # 2. Calculate frequency for each Variable
-            # Dictionary structure: likelihoods[x_variable_index][y_label][x_value]
-            # e.g. the frequency of 0 in X_2 given Y = 1 is likelihoods[2][1][0]
+        # 2. Calculate frequency for each Variable
+        # Dictionary structure: likelihoods[x_variable_index][y_label][x_value]
+        # e.g. the frequency of 0 in X_2 given Y = 1 is likelihoods[2][1][0]
+        if X_dtype is CATEGORICAL:
             for i, D_m in enumerate(D_categorical.iloc[0]):
                 if likelihoods.get(i) is None:
                     likelihoods[i] = {}
                 if likelihoods[i].get(label) is None:
                     likelihoods[i][label] = {}
+
                 for value in range(D_m):
                     sample_freq = len(class_x[class_x[i] == value])
-                    # print(f"Given label = {label}, for variable X_{i} we have {sample_freq} instances of value {value}.")
                     likelihoods[i][label][value] = (sample_freq + L) / (len(class_x[i]) + L * D_m)
+        elif X_dtype is CONTINUOUS:
+            for i in X.columns:
+                if likelihoods.get(i) is None:
+                    likelihoods[i] = {}
+                if likelihoods[i].get(label) is None:
+                    likelihoods[i][label] = {}
+                X_i: pd.Series = class_x[i]
+                mean = X_i.mean()
+                std = X_i.std()
+                likelihoods[i][label][MEAN_KEY] = mean
+                likelihoods[i][label][STD_KEY] = std
+        else:
+            raise ValueError(f"Unknown Data type provided. Please choose either \"{CATEGORICAL}\" or \"{CONTINUOUS}\"")
 
-        model = {CLASS_PROBABILITIES: class_probability,
-                 LIKELIHOODS: likelihoods}
-    elif X_dtype is CONTINUOUS:
-        pass
-    else:
-        raise ValueError("Unknown Data type provided. Please choose either \"categorical\" or \"continuous\"")
+    model = {CLASS_PROBABILITIES_KEY: class_probability,
+             LIKELIHOODS_KEY: likelihoods}
+
     return model
 
 
@@ -78,26 +94,34 @@ def predict_NBC(model, X: pd.DataFrame, X_dtype):
                          input samples.
     """
     predictions = []
-    if X_dtype is CATEGORICAL:
-        # For each sample
-        for i in range(len(X)):
-            sample = X.iloc[[i]]
-            probability_per_class = {}
-            # For each class label
-            for label in model[CLASS_PROBABILITIES].keys():
-                prob_prod = model[CLASS_PROBABILITIES][label]
-                # For each Variable X_i calculate P(Y = yk) * prod(P(X_i | Y = yk))
-                for X_i in sample.columns:
-                    x_value = sample[X_i].iloc[0]
-                    prob_prod *= model[LIKELIHOODS][X_i][label][x_value]
-                probability_per_class[label] = prob_prod
-            # argmax w.r.t. y_k from the calculated products
-            predictions.append(max(probability_per_class, key=probability_per_class.get))
-    elif X_dtype is CONTINUOUS:
-        pass
-    else:
-        raise ValueError("Unknown Data type provided. Please choose either \"categorical\" or \"continuous\"")
+
+    # For each sample
+    for i in range(len(X)):
+        sample = X.iloc[[i]]
+        probability_per_class = {}
+        # For each class label
+        for label in model[CLASS_PROBABILITIES_KEY].keys():
+            prob_prod = model[CLASS_PROBABILITIES_KEY][label]
+            # For each Variable X_i calculate P(Y = yk) * prod(P(X_i | Y = yk))
+            for X_i in sample.columns:
+                x_value = sample[X_i].iloc[0]
+                if X_dtype is CATEGORICAL:
+                    prob_prod *= model[LIKELIHOODS_KEY][X_i][label][x_value]
+                elif X_dtype is CONTINUOUS:
+                    mu = model[LIKELIHOODS_KEY][X_i][label][MEAN_KEY]
+                    sigma = model[LIKELIHOODS_KEY][X_i][label][STD_KEY]
+                    prob_prod *= gauss_pdf(x=x_value, mu=mu, sigma=sigma)
+                else:
+                    raise ValueError(
+                        f"Unknown Data type provided. Please choose either \"{CATEGORICAL}\" or \"{CONTINUOUS}\"")
+            probability_per_class[label] = prob_prod
+        # argmax w.r.t. y_k from the calculated products
+        predictions.append(max(probability_per_class, key=probability_per_class.get))
     return predictions
+
+
+def gauss_pdf(x, mu, sigma):
+    return 1 / (np.sqrt(2 * np.pi * sigma ** 2)) * np.exp((-(x - mu) ** 2) / (2 * (sigma ** 2)))
 
 
 def compute_accuracy(predictions, actual_y):
@@ -111,18 +135,18 @@ def compute_accuracy(predictions, actual_y):
 
 
 def print_model(model):
-    print(CLASS_PROBABILITIES, ':', '{')
-    for label in model[CLASS_PROBABILITIES].keys():
-        print('\t', label, ':', model[CLASS_PROBABILITIES][label])
+    print(CLASS_PROBABILITIES_KEY, ':', '{')
+    for label in model[CLASS_PROBABILITIES_KEY].keys():
+        print('\t', label, ':', model[CLASS_PROBABILITIES_KEY][label])
     print('}')
     print('------------------------------------------------------')
-    print(LIKELIHOODS, ':', '{')
-    for X_i in model[LIKELIHOODS].keys():
+    print(LIKELIHOODS_KEY, ':', '{')
+    for X_i in model[LIKELIHOODS_KEY].keys():
         print(f"\t X_{X_i} : {'{'}")
-        for label in model[LIKELIHOODS][X_i].keys():
+        for label in model[LIKELIHOODS_KEY][X_i].keys():
             print(f"\t\t Y = {label} : {'{'}")
-            for x_value in model[LIKELIHOODS][X_i][label]:
-                print(f"\t\t\t P(X_{X_i} = {x_value} | Y = {label}) : {model[LIKELIHOODS][X_i][label][x_value]}")
+            for x_value in model[LIKELIHOODS_KEY][X_i][label]:
+                print(f"\t\t\t P(X_{X_i} = {x_value} | Y = {label}) : {model[LIKELIHOODS_KEY][X_i][label][x_value]}")
             print('\t\t}')
         print('\t}')
     print('}')
@@ -139,18 +163,30 @@ def train_test_split(X, Y):
     return X_train, Y_train, X_test, Y_test
 
 
-if __name__ == "__main__":
-    X = pd.read_csv('Assignment2_Data/DatasetA_X_categorical.csv', header=None)
-    Y = pd.read_csv('Assignment2_Data/DatasetA_Y.csv', header=None)
-    D_categorical = pd.read_csv('Assignment2_Data/DatasetA_D_categorical.csv', header=None)
+def main(X_dtype):
+    if X_dtype is CATEGORICAL:
+        X = pd.read_csv(DISCRETE_X_FP, header=None)
+        Y = pd.read_csv(DISCRETE_Y_FP, header=None)
+        D_categorical = pd.read_csv(D_CATEGORICAL_FP, header=None)
+    elif X_dtype is CONTINUOUS:
+        X = pd.read_csv(CONTINUOUS_X_FP, header=None)
+        Y = pd.read_csv(CONTINUOUS_Y_FP, header=None)
+        D_categorical = None
+    else:
+        raise ValueError(f"Unknown Data type provided. Please choose either \"{CATEGORICAL}\" or \"{CONTINUOUS}\"")
     accuracies = []
     for i in tqdm(range(REPEAT)):
         X_train, y_train, X_test, y_test = train_test_split(X=X, Y=Y)
-        model = train_NBC(X=X_train, X_dtype=CATEGORICAL, Y=y_train, L=SMOOTH_STR, D_categorical=D_categorical)
-        if PRINT_MODEL:
+        model = train_NBC(X=X_train, X_dtype=X_dtype, Y=y_train, L=SMOOTH_STR, D_categorical=D_categorical)
+        if PRINT_MODEL and i == 0:
             print_model(model=model)
-        predictions = predict_NBC(model=model, X=X_test, X_dtype=CATEGORICAL)
+        predictions = predict_NBC(model=model, X=X_test, X_dtype=X_dtype)
         accuracy = compute_accuracy(predictions=predictions, actual_y=y_test.values.tolist())
         accuracies.append(accuracy)
 
     print('Average Naive Bayes Accuracy = ', np.mean(accuracies))
+
+
+if __name__ == "__main__":
+    main(X_dtype=CATEGORICAL)
+    main(X_dtype=CONTINUOUS)
